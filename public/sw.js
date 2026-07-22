dd5e0c1eb7fe8a79eada5bb9f3c73bc885a938bb
oleksiichervak-creator <oleksiichervak@gmail.com>
2026-07-22 22:12:35 +0300

    Hide admin tables until authentication

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'swine-admin-v15';
+const CACHE = 'swine-admin-v16';
 const ASSETS = ['/', '/styles.css', '/app.js', '/manifest.json'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
